d909d8a7f5458ed7efd1ace555987afc4a190fa7
Compote <simbiatoff@gmail.com>
2018-08-04 01:25:52 +0300

    finished the price-carousel

--- ts/modules/carousel-of-table.ts
+++ ts/modules/carousel-of-table.ts
@@ -2,10 +2,47 @@ import Carousel from "./carousel";
 
 export default class CarouselOfTable extends Carousel {
   public tableCells: any[];
+  public table;
+  protected onTouchStart;
+  protected onTouchMove;
+  protected onTouchEnd;
 
-  public constructor(options, tableCells) {
+  public constructor(options, table, tableCells) {
     super(options);
+    this.table = table;
     this.tableCells = [...tableCells];
+
+    this.slides.forEach((elem) => {
+      this.removeTouchEvents(elem);
+    });
+
+    this.onTouchMove = (evt) => {
+      const touch = evt.changedTouches[0];
+      this.distanсeMoveTouch = this.initTouchPositionX - touch.clientX;
+
+      if (this.distanсeMoveTouch > 20 &&
+          this.distanсeMoveTouch < 40 ||
+          this.distanсeMoveTouch < -20 &&
+          this.distanсeMoveTouch > -40
+          ) {
+            this.table.style.transform = `translateX(${-this.distanсeMoveTouch}px)`;
+      }
+    };
+
+    this.onTouchEnd = () => {
+      this.slides[this.currentSlide].classList.remove(this.touchStateSelector);
+      this.table.style.transform = "";
+
+      if (this.distanсeMoveTouch >= 30) {
+        this.toRightSlide();
+      }
+
+      if (this.distanсeMoveTouch <= -30) {
+        this.toLeftSlide();
+      }
+    };
+
+    this.addTouchEvents(this.table);
   }
 
   public activeSlide(index: number) {
@@ -17,5 +54,17 @@ export default class CarouselOfTable extends Carousel {
     for (let i = this.currentSlide; i < this.tableCells.length; i = i + 3) {
       this.tableCells[i].classList.add("price__cell--active");
     }
+  }
+
+  public addTouchEvents(elem) {
+    elem.addEventListener("touchstart", this.onTouchStart, false);
+    elem.addEventListener("touchmove", this.onTouchMove, false);
+    elem.addEventListener("touchend", this.onTouchEnd, false);
+  }
+
+  public removeTouchEvents(elem) {
+    elem.removeEventListener("touchstart", this.onTouchStart, false);
+    elem.removeEventListener("touchmove", this.onTouchMove, false);
+    elem.removeEventListener("touchend", this.onTouchEnd, false);
   }
 }
--- ts/modules/price-carousel.ts
+++ ts/modules/price-carousel.ts
@@ -5,8 +5,13 @@ const options = {
   slidesActiveStateSelector: "price__colhead--active",
   bullets: document.querySelectorAll(".price__toggle"),
   bulletsActiveStateSelector: "bullets__btn--active",
+  touchStateSelector: "price__colhead--touch",
 };
+const table = document.querySelector(".price__table");
 const tableCells = document.querySelectorAll(".price__cell");
-const priceCarousel = new CarouselOfTable(options, tableCells);
+const priceCarousel = new CarouselOfTable(options, table, tableCells);
+
+priceCarousel.activeSlide(1);
+priceCarousel.activeBullet(1);
 
 export default priceCarousel;
