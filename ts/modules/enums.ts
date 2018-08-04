dd1121c11a92317419afeb2b51e3055b631d3e83
Compote <simbiatoff@gmail.com>
2018-08-04 16:18:17 +0300

    added enums module

---
+++ ts/modules/enums.ts
@@ -0,0 +1,14 @@
+enum Page {
+  Index = 0,
+  Photo = 1,
+  Contest = 2,
+  Htmlacademy = 3,
+}
+
+enum ScreenWidth {
+  Mobile = 320,
+  Tablet = 660,
+  Desktop = 960,
+}
+
+export {Page, ScreenWidth};
