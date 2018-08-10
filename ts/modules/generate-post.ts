4c195376d1076c5c340e26307bb4eb947ceb38c7
Compote <simbiatoff@gmail.com>
2018-08-11 01:39:03 +0300

    changed template.querySelector to clone.querySelector

--- ts/modules/generate-post.ts
+++ ts/modules/generate-post.ts
@@ -2,24 +2,24 @@ export default function generatePost(data) {
   const template: any = document.querySelector(".post-template");
   const clone = document.importNode(template.content, true);
   const postImg = {
-    mobile: template.querySelector(".post-template__mobile"),
-    tablet: template.querySelector(".post-template__tablet"),
-    desktop: template.querySelector(".post-template__desktop"),
-    mobileWebp: template.querySelector(".post-template__webp-mobile"),
-    tabletWebp: template.querySelector(".post-template__webp-tablet"),
-    desktopWebp: template.querySelector(".post-template__webp-desktop"),
-    fullSize: template.querySelector(".post__link-photo"),
+    mobile: clone.querySelector(".post-template__mobile"),
+    tablet: clone.querySelector(".post-template__tablet"),
+    desktop: clone.querySelector(".post-template__desktop"),
+    mobileWebp: clone.querySelector(".post-template__webp-mobile"),
+    tabletWebp: clone.querySelector(".post-template__webp-tablet"),
+    desktopWebp: clone.querySelector(".post-template__webp-desktop"),
+    fullSize: clone.querySelector(".post__link-photo"),
   };
-  const postAuthor = template.querySelector(".post__author");
-  const postDate = template.querySelector(".post__time");
-  const postContent = template.querySelector(".post__content");
-  const postLike = template.querySelector(".post__like");
+  const postAuthor = clone.querySelector(".post__author");
+  const postDate = clone.querySelector(".post__time");
+  const postContent = clone.querySelector(".post__content");
+  const postLike = clone.querySelector(".post__like");
   const setSrcImg = (source) => {
     if (data.imgSrc[source]) {
-      postImg[source].srcset = data.imgSrc[source];
+      postImg[source].setAttribute("srcset", data.imgSrc[source]);
 
       if (data.imgSrc[source + "2x"]) {
-        postImg[source].srcset = data.imgSrc[source] + " " + data.imgSrc[source + "2x"];
+        postImg[source].setAttribute("srcset", data.imgSrc[source] + ", " + data.imgSrc[source + "2x"]);
       }
     } else {
       postImg[source].remove();
@@ -29,7 +29,7 @@ export default function generatePost(data) {
   postImg.mobile.src = data.imgSrc.mobile;
 
   if (data.imgSrc.mobile2x) {
-    postImg.mobile.srcset = data.imgSrc.mobile2x;
+    postImg.mobile.setAttribute("srcset", data.imgSrc.mobile2x);
   } else {
     postImg.mobile.removeAttribute("srcset");
   }
@@ -49,6 +49,5 @@ export default function generatePost(data) {
   postDate.datetime = data.postDate;
   postContent.textContent = data.postContent;
   postLike.textContent = `Нравится: ${data.likeAmount}`;
-
   return clone;
 }
