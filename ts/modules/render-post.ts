a47b10c2424e989c4becdd05d1016429057fa043
Compote <simbiatoff@gmail.com>
2018-08-10 23:53:31 +0300

    changed function declaration to function expression

--- ts/modules/render-post.ts
+++ ts/modules/render-post.ts
@@ -1,22 +1,24 @@
 import generatePost from "./generate-post";
 
-export default function renderPost(postAmountRender: number,
-                                   dataPosts: any[],
-                                   postParent) {
-    let alreadyRendered = 0;
-    const fragment = document.createDocumentFragment();
+const renderPost = (postAmountRender: number,
+                    dataPosts: any[],
+                    postParent) => {
+  let alreadyRendered = 0;
+  const fragment = document.createDocumentFragment();
 
-    return () => {
-      const isAllRender = (i: number): boolean => {
-        return i < postAmountRender + alreadyRendered && i < dataPosts.length;
-      };
-
-      for (let i = alreadyRendered; isAllRender(i); i++) {
-        const post = generatePost(dataPosts[i]);
-        fragment.appendChild(post);
-      }
-
-      alreadyRendered += postAmountRender;
-      postParent.appendChild(fragment);
+  return () => {
+    const isAllRender = (i: number): boolean => {
+      return i < postAmountRender + alreadyRendered && i < dataPosts.length;
     };
-}
+
+    for (let i = alreadyRendered; isAllRender(i); i++) {
+      const post = generatePost(dataPosts[i]);
+      fragment.appendChild(post);
+    }
+
+    alreadyRendered += postAmountRender;
+    postParent.appendChild(fragment);
+  };
+};
+
+export default renderPost;
