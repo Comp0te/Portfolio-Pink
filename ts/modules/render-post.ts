ed9e89ef5e7fdb0ca465ca79f48e2c485845f83a
Compote <simbiatoff@gmail.com>
2018-08-12 01:15:22 +0300

    added the ability to render panorama-post

--- ts/modules/render-post.ts
+++ ts/modules/render-post.ts
@@ -1,21 +1,31 @@
 import generatePost from "./generate-post";
 
-let alreadyRendered = 0;
+let alreadyRenderedPost = 0;
+let alreadyRenderedPanorama = 0;
 
 const renderPost = (postAmountRender: number,
                     dataPosts: any[],
-                    postParent) => {
+                    postParent,
+                    isPanorama: boolean) => {
   const fragment = document.createDocumentFragment();
+  const AlreadyRender = () => {
+    return isPanorama ? alreadyRenderedPanorama : alreadyRenderedPost;
+  };
   const isAllRender = (i: number): boolean => {
-      return i < postAmountRender + alreadyRendered && i < dataPosts.length;
+      return i < postAmountRender + AlreadyRender() && i < dataPosts.length;
     };
 
-  for (let i = alreadyRendered; isAllRender(i); i++) {
-      const post = generatePost(dataPosts[i]);
+  for (let i = AlreadyRender(); isAllRender(i); i++) {
+      const post = generatePost(dataPosts[i], isPanorama);
       fragment.appendChild(post);
     }
 
-  alreadyRendered += postAmountRender;
+  if (!isPanorama) {
+    alreadyRenderedPost += postAmountRender;
+  } else {
+    alreadyRenderedPanorama += postAmountRender;
+  }
+
   postParent.appendChild(fragment);
 };
 
