02a3e2d7e5a176ad68a8785127921d81704a3a31
Compote <simbiatoff@gmail.com>
2018-08-12 01:10:01 +0300

    reworked to function getData

--- ts/modules/fetch-data.ts
+++ ts/modules/fetch-data.ts
@@ -1,28 +1,23 @@
-import jsonPosts from "./json-posts";
-
-const getPostData = () => {
-  return fetch("/server/posts/", {
-    method: "GET",
-    mode: "same-origin",
-    headers: {
-      "Content-type": "application/json; charset=utf-8",
-    },
+export default function getData(url: string,
+                                options,
+                                errorContainer,
+                                dataWithoutServer: string) {
+  return fetch(url, {
+    method: options.method,
+    mode: options.mode,
+    headers: options.headers,
   })
     .then((response) => {
       if (response.ok) {
         return response.json();
       } else {
-        return JSON.parse(jsonPosts); // Due to lack of back end.
+        return JSON.parse(dataWithoutServer); // Due to lack of back end.
         // throw new Error("ошибка загрузки информации");
       }
     })
     .catch((error) => {
-      const errorContainer: any = document.querySelector(".photo__list");
-
       errorContainer.style.textAlign = "center";
       errorContainer.style.fontSize = "24px";
       errorContainer.textContent = `1) Возникла ${error.message} с сервера, попробуйте перезагрузить страницу`;
     });
-};
-
-export default getPostData;
+}
