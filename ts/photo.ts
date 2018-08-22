d0491d795a484c8190ea258c165ff5ad78765a27
Compote <simbiatoff@gmail.com>
2018-08-23 00:23:30 +0300

    finished photo.html scripts

--- ts/photo.ts
+++ ts/photo.ts
@@ -1,6 +1,8 @@
 import {Page} from "./modules/enums";
+import "./modules/mobile-tabs-slider";
 import headerMenu from "./modules/page-header";
 import "./modules/panorama-post";
 import "./modules/post";
+import "./modules/upload-form-submit";
 
 headerMenu.menuItemActive = Page.Photo;
