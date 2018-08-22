b72c229a934dfc7395944a57e5cb6175a825ee6b
Compote <simbiatoff@gmail.com>
2018-08-23 00:22:11 +0300

    renamed and finished the upload-form-submit module

--- ts/modules/upload-comment-handling.ts
+++
@@ -1,8 +0,0 @@
-import {toggleFormState} from "./upload-photo-handling";
-
-const buttonBack = document.querySelector(".comment__reset");
-const uploadForm = document.querySelector("upload__form");
-
-buttonBack.addEventListener("click", () => {
-  toggleFormState();
-}, false);
---
+++ ts/modules/upload-form-submit.ts
@@ -0,0 +1,69 @@
+import postUploadForm from "./post-upload-form";
+import {fillFieldsError} from "./upload-dialog";
+import {isFormStatePhoto, onClickResetButton, photoData, toggleFormState} from "./upload-photo-handling";
+
+const buttonBack = document.querySelector(".comment__reset");
+const uploadForm: any = document.querySelector(".upload__form");
+const commetAuthorElem: any = document.querySelector(".comment__name-input");
+const commentMessageElem: any = document.querySelector(".comment__textarea");
+const commentData = {
+  author: "",
+  message: "",
+};
+
+buttonBack.addEventListener("click", onClickBackButton, false);
+uploadForm.addEventListener("submit", onSubmit, false);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  if (!isFormStatePhoto) {
+    if (commetAuthorElem.validity.valueMissing ||
+      commentMessageElem.validity.valueMissing) {
+      fillFieldsError.showDialog();
+      return;
+    } else {
+      getCommentData();
+      postUploadForm(generateFormData(photoData, commentData))
+      .then((response) => {
+        if (response) {
+        resetForm();
+        }
+      });
+    }
+  }
+}
+
+function onClickBackButton() {
+  toggleFormState();
+}
+
+function getCommentData() {
+  commentData.author = commetAuthorElem.value;
+  commentData.message = commentMessageElem.value;
+}
+
+function generateFormData(photo, comment) {
+  const formData = new FormData();
+
+  for (const key in photo) {
+    if (photo.hasOwnProperty(key)) {
+      formData.append(key, photo[key]);
+    }
+  }
+
+  for (const key in comment) {
+    if (comment.hasOwnProperty(key)) {
+      formData.append(key, comment[key]);
+    }
+  }
+
+  return formData;
+}
+
+function resetForm() {
+  commetAuthorElem.value = null;
+  commentMessageElem.value = null;
+  toggleFormState();
+  onClickResetButton();
+}
