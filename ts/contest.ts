e1cbc48978e4a22c64cb91d7f2d76480ff1463bb
Compote <simbiatoff@gmail.com>
2018-08-28 21:36:43 +0300

    added onChangeScreenWidth event

--- ts/contest.ts
+++ ts/contest.ts
@@ -1,5 +1,17 @@
 import "./modules/contest-form-submit";
-import {Page} from "./modules/enums";
+import {Page, ScreenWidth} from "./modules/enums";
 import headerMenu from "./modules/page-header";
 
+const contactsTitle = document.querySelector(".form-contacts__title");
+const onChangeScreenWidth = () => {
+  if (window.innerWidth < ScreenWidth.Tablet) {
+    contactsTitle.classList.add("visually-hidden");
+  } else {
+    contactsTitle.classList.remove("visually-hidden");
+  }
+};
+
 headerMenu.menuItemActive = Page.Contest;
+
+onChangeScreenWidth();
+window.addEventListener("resize", onChangeScreenWidth, false);
