3b81382bf96b7efb88661547ee3d38bfc2df5e6a
Compote <simbiatoff@gmail.com>
2018-08-28 21:39:44 +0300

    added userEmail.validity.typeMismatch and invalid state

--- ts/modules/contest-form-submit.ts
+++ ts/modules/contest-form-submit.ts
@@ -1,4 +1,4 @@
-import {showfillFieldsError} from "./contest-dialog";
+import {showValidityError} from "./contest-dialog";
 import postContestForm from "./post-contest-form";
 
 const contestForm: any = document.querySelector(".contest__form");
@@ -6,6 +6,10 @@ const userName: any = document.querySelector("#name");
 const userSurname: any = document.querySelector("#surname");
 const userEmail: any = document.querySelector("#email");
 const submitButton: any = document.querySelector(".contest__submit-button");
+const emailError = {
+  title: "Неверный адрес электронной почты",
+  message: "Ввдети адрес электронной почты в формате user@example.com",
+};
 
 contestForm.addEventListener("submit", onSubmit, false);
 
@@ -15,12 +19,25 @@ function onSubmit(evt) {
   if (userName.validity.valueMissing ||
     userSurname.validity.valueMissing ||
     userEmail.validity.valueMissing) {
-    showfillFieldsError();
+    showValidityError();
+
+    userName.classList.add("invalid");
+    userSurname.classList.add("invalid");
+    userEmail.classList.add("invalid");
+    return;
+  } else if (userEmail.validity.typeMismatch) {
+    showValidityError(emailError.title, emailError.message);
+
+    userEmail.classList.add("invalid");
     return;
   } else {
     const formData = new FormData(contestForm);
 
+    userName.classList.remove("invalid");
+    userSurname.classList.remove("invalid");
+    userEmail.classList.remove("invalid");
     submitButton.setAttribute("disabled", "");
+
     postContestForm(formData)
     .then((response) => {
       if (response) {
